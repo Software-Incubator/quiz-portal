544994de8d60416a579cedf065f86a33f3472e24
Ashutosh Baghel <ashutosh@simplifii.com>
2018-06-04 23:11:16 +0530

    model setup

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+# class User_details(User):
+#     name = models.CharField(max_length=100, blank=True, default='')
+#     email = models.EmailField(max_length=70,blank=False)
+#     # password = models.CharField(max_legnth=250, widget=serializers.PasswordInput)
+#     Token = models.CharField(max_length=200,blank=False)
+
+class Payment_details(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_amount = models.IntegerField(blank=False)
+    order_id = models.CharField(max_length=100, blank=False)
+    
+class Customer(models.Model):
+    customer_id = models.CharField(max_length=100, blank=False)
+
+# class Meter_details(models.Model):
+#     meter_no = models.IntegerField(blank=False)
+#     elec_remain = models.IntegerField(blank=False, default = 0)
--- quiz_portal/settings.py
+++ quiz_portal/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'quiz_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quiz_portal',
+        'USER': 'quizuser',
+        'PASSWORD': 'quizportal@SI',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators

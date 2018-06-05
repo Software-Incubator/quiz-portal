b6480ac26c88482fbc8d2ea32c015aecaeecac32
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2018-06-05 10:24:51 +0530

    Update .gitignore file

--- quiz_portal/settings.py
+++ quiz_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pi6^-ib=xwu(a6hg7@*+#ozhh%q^qg(hafeg^hk=7k)bpp!&5l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'quiz_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quiz_portal',
+        'USER': 'quizuser',
+        'PASSWORD': 'quizportal@SI',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

d137836fdef001e81381a44ef8ae916f6cf28c81
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2018-06-06 16:04:44 +0530

    Change models

--- core/models.py
+++ core/models.py
@@ -2,35 +2,61 @@ from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 
 class Test(models.Model):
-    Tname = models.CharField(max_length=100, blank=False)
+    test_name = models.CharField(max_length=100, blank=False)
     duration = models.PositiveIntegerField(blank = False)
 
+
+
 class Category(models.Model):
-    Tname = models.ForeignKey(Test, on_delete=models.CASCADE)
-    Cname = models.CharField(max_length=100, blank=False)
+    category = models.CharField(max_length=225)
 
-class Questions(models.Model):
-    Cname = models.ForeignKey(Category, on_delete=models.CASCADE)
-    Question = RichTextUploadingField()
-    Choice1 = RichTextUploadingField()
-    Choice2 = RichTextUploadingField()
-    Choice3 = RichTextUploadingField()
-    Choice4 = RichTextUploadingField()
-    right_choice = RichTextUploadingField()
+    class Meta:
+        verbose_name_plural = "Categories"
 
-class Instructions(models.Model):
-    Tname = models.ForeignKey(Test, on_delete=models.CASCADE)
-    Instruction = RichTextUploadingField()
-
-class Student_details(models.Model):
-    Name = models.CharField(max_length=100, blank=True)
-    Father_name = models.CharField(max_length=100, blank=True)
-    Email = models.EmailField(blank = False)
-    Phone_number = models.CharField(max_length=100, blank=True)
-
-class Questions_Answer(models.Model):
-    Email = models.ForeignKey(Student_details, on_delete=models.CASCADE)
-    Question = models.ForeignKey(Questions, on_delete=models.CASCADE)
-    selected_choice = RichTextUploadingField()
+    def __str__(self):
+        return "Category = %s" % self.category
 
 
+class Question(models.Model):
+    question_text = RichTextUploadingField()
+    negative = models.BooleanField()
+    negative_marks = models.IntegerField(null=True)
+    marks = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "<Question: %s>" % self.question_text
+
+
+class QuestionChoice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = RichTextUploadingField()
+    def __str__(self):
+        return "<Choice = %s>" % self.choice
+
+
+class CorrectChoice(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE,
+                                    db_column='question_id')
+    correct_choice = models.ForeignKey(QuestionChoice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "<Correct choice = %s>" % self.correct_choice
+
+
+class Instruction(models.Model):
+    instruction = RichTextUploadingField()
+
+
+# class Category(models.Model):
+#     test_name = models.ForeignKey(Test, on_delete=models.CASCADE)
+#     category_name = models.CharField(max_length=100, blank=False)
+#
+# class Question(models.Model):
+#     category_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+#     question = RichTextUploadingField()
+#     choice1 = RichTextUploadingField()
+#     choice2 = RichTextUploadingField()
+#     choice3 = RichTextUploadingField()
+#     choice4 = RichTextUploadingField()
+#     right_choice = RichTextUploadingField()

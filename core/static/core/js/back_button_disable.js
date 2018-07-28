4c897407f0587acde22b4ffc05ebaea97b2c926e
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2018-07-29 01:13:26 +0530

    Change url references

--- core/static/core/js/back_button_disable.js
+++ core/static/core/js/back_button_disable.js
@@ -1,3 +1,7 @@
 function preventBack(){window.history.forward();}
     setTimeout("preventBack()", 0);
     window.onunload=function(){null};
+
+window.location.hash="no-back-button";
+window.location.hash="Again-No-back-button";
+window.onhashchange=function(){window.location.hash="#";}

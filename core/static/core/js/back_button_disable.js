5785fa4cefb3d81588ccad572abcd56872f92b2e
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2018-07-28 23:23:14 +0530

    Disable back button

--- core/static/core/js/back_button_disable.js
+++ core/static/core/js/back_button_disable.js
@@ -1,3 +1,3 @@
-window.location.hash="no-back-button";
-window.location.hash="Again-No-back-button";
-window.onhashchange=function(){window.location.hash="#";};
+function preventBack(){window.history.forward();}
+    setTimeout("preventBack()", 0);
+    window.onunload=function(){null};

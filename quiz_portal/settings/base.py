8384690259cf2b7ddb5d6eba4b58fe37f433c38f
mr-star-k <pramodjaiswal517@gmail.com>
2019-07-03 14:26:38 +0530

    travis build

--- quiz_portal/settings/base.py
+++ quiz_portal/settings/base.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('QUIZ_PORTAL_SECRET_KEY')
 
-# SECURITY WARNING: don't ru    n with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['silive.in', 'www.silive.in', 'localhost', '*']

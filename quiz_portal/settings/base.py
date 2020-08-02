f14b66f18e827c232973e40673079c041cf6e605
Stark-Pramod <pramodjaiswal517@gmail.com>
2020-08-02 17:12:14 +0530

    Ready to fork code

--- quiz_portal/settings/base.py
+++ quiz_portal/settings/base.py
@@ -9,7 +9,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('QUIZ_PORTAL_SECRET_KEY')
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
